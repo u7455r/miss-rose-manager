e2e6d4905db3d13a96c473f3060c88e68d7f3b06
u7455r <didarulpk11@gmail.com>
2026-09-06 14:44:48 +0600

    Update rose_bot.py

--- rose_bot.py
+++ rose_bot.py
@@ -9,7 +9,7 @@ from keep_alive import keep_alive
 
 print("--- Initializing Rafim Rose Manager Ultimate Engine ---", flush=True)
 
-BOT_TOKEN = "8886219226:AAEFZjj_SJ6fduS6toHzZBrxxtwJhi9rKYc"
+BOT_TOKEN = "8886219226:AAGue3mFnx5NWCToUqTbkUxf9Ah_ThQ-YaQ"
 bot = telebot.TeleBot(BOT_TOKEN, threaded=True)
 
 REQ_CHANNEL = "@rafimhossen3"
